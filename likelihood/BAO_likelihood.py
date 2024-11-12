166bb74285e1a6bf6bc723200444e2d27111c003
Chiara De Leo <chiara.deleo@students.uniroma2.eu>
2024-11-12 11:43:39 +0100

    updated BAO mock

--- likelihood/BAO_likelihood.py
+++ likelihood/BAO_likelihood.py
@@ -8,10 +8,8 @@ class BAOLike(Likelihood):
     
     def initialize(self):
         
-        self.dataset_DH = np.load(self.BAO_data_path+'_DH.npy',allow_pickle=True).item()
-        self.dataset_DM = np.load(self.BAO_data_path+'_DM.npy',allow_pickle=True).item()
-        self.dataset_DV = np.load(self.BAO_data_path+'_DV.npy',allow_pickle=True).item()
         self.dataset_DHDM = np.load(self.BAO_data_path+'_DHDM.npy',allow_pickle=True).item()
+        self.dataset_DV = np.load(self.BAO_data_path+'_DV.npy',allow_pickle=True).item()
 
         zmax = 4.
         self.z_camb = np.linspace(0.001, zmax, 10000)
@@ -27,14 +25,13 @@ class BAOLike(Likelihood):
     
     def logp(self, **params_values): 
 
-        diffvec_DH = self.provider.get_result('DH')(self.dataset_DH['z'])-self.dataset_DH['value']
-        diffvec_DM = self.provider.get_result('DM')(self.dataset_DM['z'])-self.dataset_DM['value']
-        diffvec_DV = self.provider.get_result('DV')(self.dataset_DV['z'])-self.dataset_DV['value']
+        diffvec_DH = self.provider.get_result('DH')(self.dataset_DHDM['z'])-self.dataset_DHDM['DH']
+        diffvec_DM = self.provider.get_result('DM')(self.dataset_DHDM['z'])-self.dataset_DHDM['DM']
+        diffvec_DV = self.provider.get_result('DV')(self.dataset_DV['z'])-self.dataset_DV['DV']
         diffvec_DHDM=np.concatenate((diffvec_DH, diffvec_DM),axis=0)
+        chi2=np.dot((diffvec_DHDM),np.dot(np.linalg.inv((self.dataset_DHDM['covmat'])),(diffvec_DHDM)))
+        chi2+=np.dot((diffvec_DV),np.dot(np.linalg.inv(((self.dataset_DV['covmat']))),(diffvec_DV)))
 
-        chi_DHDM=np.dot((diffvec_DHDM),np.dot(np.linalg.inv((self.dataset_DHDM['covmat'])),(diffvec_DHDM)));
-        chi_DV=np.dot((diffvec_DV),np.dot(np.linalg.inv(((self.dataset_DV['covmat']))),(diffvec_DV)))
-
-        loglike=-0.5*chi_DV-0.5*chi_DHDM
+        loglike=-0.5*chi2
 
         return loglike
