7fb47a9af3f4dbeefde7e455a66044fbadf3342c
chiaradeleo1 <chiara.deleo@students.uniroma2.eu>
2024-12-13 16:19:54 +0100

    Improved DDR_theory

--- theory_code/DDR_theory.py
+++ theory_code/DDR_theory.py
@@ -11,31 +11,21 @@ from scipy.interpolate import interp1d
 
 class DDRCalcs:
 
-    def __init__(self,model,params,zcalc):
+    def __init__(self,settings,params,zcalc):
 
         self.zcalc = zcalc
-
-        if model == 'constant':
-            self.eta_EM = self.get_eta_constant(params['epsilon0_EM'])
-            
-            self.eta_GW = self.get_eta_constant(params['epsilon0_GW'])
-        
-        elif model == 'padè':
-            self.eta_EM = self.get_eta_pade_approximant(params['epsilon0_EM'])
-            
-            self.eta_GW = self.get_eta_pade_approximant(params['epsilon0_GW'])
-            
+        self.settings = settings
+        if settings['epsilon_model'] == 'constant':
+            self.eta_EM = self.get_eta(params['epsilon0_EM'])
+            self.eta_GW = self.get_eta(params['epsilon0_GW'])
         else:
             sys.exit('Unknown DDR breaking model: {}'.format(model))
 
-    def get_eta_constant(self,epsilon):
-
-        eta = interp1d(self.zcalc,(1+self.zcalc)**epsilon)
-
-        return eta
-    
-    def get_eta_pade_approximant(self, epsilon):
-
-        eta = interp1d(self.zcalc, 1 + (epsilon*np.log(1+self.zcalc))/(1-(epsilon*0.5*np.log(1+self.zcalc))+ (epsilon/12 * (np.log(1+self.zcalc))**2) ))
+    def get_eta(self,epsilon):
         
+        if self.settings['pade'] == 'True':
+            eta = interp1d(self.zcalc, 1 + (epsilon*np.log(1+self.zcalc))/(1-(epsilon*0.5*np.log(1+self.zcalc))+ (epsilon/12 * (np.log(1+self.zcalc))**2) ))
+        else:
+            eta = interp1d(self.zcalc,(1+self.zcalc)**epsilon)
+
         return eta
