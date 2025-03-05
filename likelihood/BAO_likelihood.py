edf472cb63a00a5f3b08cbfbe603a052d7a2a1c3
Chiara De Leo <chiara.deleo@students.uniroma2.eu>
2025-03-05 16:27:31 +0100

    Added options in BAO_likelihood to use DM and DH only, or DV only

--- likelihood/BAO_likelihood.py
+++ likelihood/BAO_likelihood.py
@@ -7,15 +7,25 @@ from cobaya.likelihood import Likelihood
 class BAOLike(Likelihood):
     
     def initialize(self):
-        
-        self.dataset_DHDM = pd.read_csv(self.BAO_data_path+'_data_DHDM.txt',sep='\s+',header=0)
-        covmat_DHDM       = pd.read_csv(self.BAO_data_path+'_covmat_DHDM.txt',sep='\s+',header=0)
-        self.invcov_DHDM  = np.linalg.inv(covmat_DHDM)
 
-        self.dataset_DV = pd.read_csv(self.BAO_data_path+'_data_DV.txt',sep='\s+',header=0)
-        covmat_DV       = pd.read_csv(self.BAO_data_path+'_covmat_DV.txt',sep='\s+',header=0)
-        self.invcov_DV  = np.linalg.inv(covmat_DV)
+        self.data = []
+        try:
+            self.dataset_DHDM = pd.read_csv(self.BAO_data_path + '_data_DHDM.txt', sep='\s+', header=0)
+            covmat_DHDM = pd.read_csv(self.BAO_data_path + '_covmat_DHDM.txt', sep='\s+', header=0)
+            self.invcov_DHDM = np.linalg.inv(covmat_DHDM)
+            self.data.append('DHDM')
+        except FileNotFoundError as e:
+            print(f"Attention!! DH and DM files are missing.")
 
+    
+        try:
+            self.dataset_DV = pd.read_csv(self.BAO_data_path+'_data_DV.txt',sep='\s+',header=0)
+            covmat_DV       = pd.read_csv(self.BAO_data_path+'_covmat_DV.txt',sep='\s+',header=0)
+            self.invcov_DV  = np.linalg.inv(covmat_DV)
+            self.data.append('DV')
+        except FileNotFoundError as e:
+            print(f"Attention!! DV files are missing.")
+       
         if self.use_noisy_data:
             self.suffix = '_noisy'
         else:
@@ -35,14 +45,18 @@ class BAOLike(Likelihood):
         return requirements
     
     def logp(self, **params_values): 
+        chi2 = 0
+        if 'DHDM' in self.data:
+            diffvec_DH   = self.provider.get_result('DH')(self.dataset_DHDM['z'])-self.dataset_DHDM['DH'+self.suffix]
+            diffvec_DM   = self.provider.get_result('DM')(self.dataset_DHDM['z'])-self.dataset_DHDM['DM'+self.suffix]
+            diffvec_DHDM = np.concatenate((diffvec_DH, diffvec_DM),axis=0)
+            chi2  = np.dot((diffvec_DHDM),np.dot(self.invcov_DHDM,(diffvec_DHDM)))
+        if 'DV' in self.data:
+            diffvec_DV   = self.provider.get_result('DV')(self.dataset_DV['z'])-self.dataset_DV['DV'+self.suffix]
+            chi2 += np.dot((diffvec_DV),np.dot(self.invcov_DV,(diffvec_DV)))
 
-        diffvec_DH   = self.provider.get_result('DH')(self.dataset_DHDM['z'])-self.dataset_DHDM['DH'+self.suffix]
-        diffvec_DM   = self.provider.get_result('DM')(self.dataset_DHDM['z'])-self.dataset_DHDM['DM'+self.suffix]
-        diffvec_DV   = self.provider.get_result('DV')(self.dataset_DV['z'])-self.dataset_DV['DV'+self.suffix]
-        diffvec_DHDM = np.concatenate((diffvec_DH, diffvec_DM),axis=0)
-
-        chi2  = np.dot((diffvec_DHDM),np.dot(self.invcov_DHDM,(diffvec_DHDM)))
-        chi2 += np.dot((diffvec_DV),np.dot(self.invcov_DV,(diffvec_DV)))
+        
+        
 
         loglike = -0.5*chi2
 
