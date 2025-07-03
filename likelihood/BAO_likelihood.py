fixed BAO covariance transform. Allowed for fiducial from file

likelihood/BAO_likelihood.py
@@ -10,27 +10,73 @@ class BAOLike(Likelihood):
     def initialize(self):
 
         if self.DESI_table:
-            self.data_table = pd.read_csv(self.BAO_data_path+'.txt',sep='\t',header=0)
+            data_table = pd.read_csv(self.BAO_data_path+'.txt',sep='\t',header=0)
+
+            #Single observable points
+            single_obs = data_table[data_table.isnull().any(axis=1)]
+
+            data_table = data_table.dropna().reset_index(drop=True)
+            self.z_vec = data_table['z'].values
+
             if self.observables == 'distances':
-                covmat = pd.read_csv(self.BAO_data_path+'_distances_covmat.txt',sep='\t',header=0)
-                covmat.index = covmat.columns
-                self.inv_covmat = pd.DataFrame(np.linalg.inv(covmat.values),columns=covmat.columns,index=covmat.index)
+                distance_covs = self.get_covmats(data_table)
+                self.single_data = single_obs[['z','DV_rd','DV_rd_err']]
+                self.datavectors = [np.array([row['DV_rd'],row['DM_DH']]) for ind,row in data_table.iterrows()]
+                self.inv_covmats = [pd.DataFrame(np.linalg.inv(covmat.values),
+                                                 columns=covmat.columns,index=covmat.index) for covmat in distance_covs]
             elif self.observables == 'alphas':
-                covmat = pd.read_csv(self.BAO_data_path+'_alphas_covmat.txt',sep='\t',header=0)
-                covmat.index = covmat.columns
-                self.inv_covmat = pd.DataFrame(np.linalg.inv(covmat.values),columns=covmat.columns,index=covmat.index)
+                self.single_data = single_obs[['z','alpha_iso','alpha_iso_err']]
+                self.datavectors = [np.array([row['alpha_iso'],row['alpha_AP']]) for ind,row in data_table.iterrows()]
+                alpha_covs = self.transform_covmat(data_table)
+                self.inv_covmats = [pd.DataFrame(np.linalg.inv(covmat.values),
+                                                 columns=covmat.columns,index=covmat.index) for covmat in alpha_covs]
         else:
             sys.exit('UNKNOWN DATA FORMAT')
-       
+
         zmax = 4.
         self.z_camb = np.linspace(0.001, zmax, 10000)
 
+    def transform_covmat(self,data):
+        #MM to be changed
+
+        z_covs = []
+        for ind,row in data.iterrows():
+            alpha_cols = ['alpha_iso_{}'.format(ind),'alpha_AP_{}'.format(ind)]
+
+            precov   = pd.DataFrame([[row['DM_rd_err']**2,row['r_MH']*row['DM_rd_err']*row['DH_rd_err']],
+                                     [row['r_MH']*row['DM_rd_err']*row['DH_rd_err'],row['DH_rd_err']**2]],
+                                    columns=['DM_rd_{}'.format(ind),'DH_rd_{}'.format(ind)],
+                                    index=['DM_rd_{}'.format(ind),'DH_rd_{}'.format(ind)])
+
+            x1   = row['DM_rd']
+            x2   = row['DH_rd']
+            x1_F = row['DM_rd_fid']
+            x2_F = row['DH_rd_fid']
+
+            jacobian = [[(1/3)*(x1**2*x2)**(-2/3)*2*x1*x2/(x1_F**2*x2_F)**(1/3),(1/3)*(x1**2*x2)**(-2/3)*x1**2/(x1_F**2*x2_F)**(1/3)],
+                        [-(x1_F/x2_F)*(x2/x1**2),(x1_F/x2_F)*1/x1]]
+
+            z_covs.append(pd.DataFrame(jacobian @ precov.values @ np.transpose(jacobian),
+                                       columns = alpha_cols,index=alpha_cols))
+
+
+        return z_covs
+
+    def get_covmats(self,data):
+
+        z_covs = [pd.DataFrame([[row['DV_rd_err']**2,row['r_VMH']*row['DV_rd_err']*row['DM_DH_err']],
+                                [row['r_VMH']*row['DV_rd_err']*row['DM_DH_err'],row['DM_DH_err']**2]],
+                  columns=['DV_rd_{}'.format(ind),'DM_DH_{}'.format(ind)],
+                  index=['DV_rd_{}'.format(ind),'DM_DH_{}'.format(ind)]) for ind,row in data.iterrows()]
+
+        return z_covs
 
     def get_requirements(self):
         # Requirements are the output of the theory code that you are using
-        requirements = {'DM': None,
-                        'DH': None,
-                        'DV': None,
+        requirements = {'DM_DH': None,
+                        'DV_rd': None,
+                        'alpha_iso': None,
+                        'alpha_AP': None,
                         'rdrag': None}
 
         return requirements
@@ -41,47 +87,20 @@ class BAOLike(Likelihood):
         rdrag = params_values['rdrag']
 
         if self.observables == 'distances':
-            DV_only     = self.data_table[self.data_table.isna().any(axis=1)]
-            chi2_single = (DV_only['DV/rd']-self.provider.get_result('DV')(DV_only['z'])/params_values['rdrag'])**2/DV_only['DV_err']**2
-
-            allobs = self.data_table[~self.data_table.isna().any(axis=1)]
-
-            split_cols = [col.split('_') for col in self.inv_covmat.columns]
-            datavec    = np.array([allobs.iloc[int(ind)-1][obs] for obs,ind in split_cols])
-            thvec = []
-            for obs,ind in split_cols:
-                if obs == 'DV/rd':
-                    thvec.append(self.provider.get_result('DV')(allobs.iloc[int(ind)-1]['z'])/rdrag)
-                elif obs == 'DM/DH':
-                    thvec.append(self.provider.get_result('DM')(allobs.iloc[int(ind)-1]['z'])/self.provider.get_result('DH')(allobs.iloc[int(ind)-1]['z']))
-
-            theoryvec = np.array(thvec)
-
-            chi2_all = np.dot(theoryvec-datavec,np.dot(self.inv_covmat,theoryvec-datavec))
-
-            chi2 = chi2_all+chi2_single
-
+            obs = 'DV_rd'
         elif self.observables == 'alphas':
-            iso_only     = self.data_table[self.data_table.isna().any(axis=1)]
-            chi2_single = (iso_only['alpha_iso']-(self.provider.get_result('DV')(iso_only['z'])/params_values['rdrag'])/iso_only['DV/rd_fid'])**2/iso_only['iso_err']**2
+            obs = 'alpha_iso'
+        chi2_single = sum([(row[obs]-self.provider.get_result(obs)(row['z']))**2/row[obs+'_err']**2 for ind,row in self.single_data.iterrows()])
 
-            allobs = self.data_table[~self.data_table.isna().any(axis=1)]
+        chi2_vec = []
+        for z,data,invcov in zip(self.z_vec,self.datavectors,self.inv_covmats):
+            obs = ['_'.join(item for item in col.split('_')[:2]) for col in invcov.columns]
+            theoryvec = np.array([self.provider.get_result(o)(z) for o in obs])
+            chi2_vec.append(np.dot(theoryvec-data,np.dot(invcov,theoryvec-data)))
 
-            split_cols = [col.split('_') for col in self.inv_covmat.columns]
-            datavec    = np.array([allobs.iloc[int(ind)-1][obs1+'_'+obs2] for obs1,obs2,ind in split_cols])
-            thvec = []
-            for obs1,obs2,ind in split_cols:
-                if obs1+'_'+obs2 == 'alpha_iso':
-                    thvec.append((self.provider.get_result('DV')(allobs.iloc[int(ind)-1]['z'])/rdrag)/allobs.iloc[int(ind)-1]['DV/rd_fid'])
-                elif obs1+'_'+obs2 == 'alpha_AP':
-                    thvec.append((self.provider.get_result('DH')(allobs.iloc[int(ind)-1]['z'])/
-                                  self.provider.get_result('DM')(allobs.iloc[int(ind)-1]['z']))/allobs.iloc[int(ind)-1]['DM/DH_fid'])
+        chi2_binned = sum(chi2_vec)
 
-            theoryvec = np.array(thvec)
-
-            chi2_all = np.dot(theoryvec-datavec,np.dot(self.inv_covmat,theoryvec-datavec))
-
-            chi2 = chi2_all+chi2_single
+        chi2 = chi2_binned+chi2_single
 
 
         loglike = -0.5*chi2

theory_code/distance_theory.py
@@ -15,7 +15,7 @@ clight = 299792.458
 
 class TheoryCalcs:
 
-    def __init__(self,settings,cosmosets,SNmodel,DDR=None,feedback=False):
+    def __init__(self,settings,cosmosets,SNmodel,fiducial,DDR=None,feedback=False):
         
         ##################
         #General settings#
@@ -59,6 +59,35 @@ class TheoryCalcs:
         #Changing theory dict elements into class attributes
         for k, v in cosmo_results.items():
             setattr(self,k,v)
+
+        #MM!!! Put here switch to use external rdrag
+        #self.rdrag = something
+
+        ################
+        #BAO quantities#
+        ################
+        if feedback:
+            print('')
+            print('Computing LCDM fiducial')
+
+        if type(fiducial) == str:
+            fidtable = pd.read_csv(fiducial,header=0,sep='\t')
+            fidcosmo = {'DV_rd': interp1d(fidtable['z'],fidtable['DV_rd']),
+                        'DH_DM': interp1d(fidtable['z'],fidtable['DH_DM'])}
+
+            self.alpha_iso = lambda x: (self.DV(x)/self.rdrag)/fidcosmo['DV_rd'](x)
+            self.alpha_AP  = lambda x: (self.DH(x)/self.DM(x))/fidcosmo['DH_DM'](x)
+
+        elif type(fiducial) == dict:
+            fidcosmo = self.call_camb(fiducial)
+
+            self.alpha_iso = lambda x: (self.DV(x)/self.rdrag)/(fidcosmo['DV'](x)/fidcosmo['rdrag']) 
+            self.alpha_AP  = lambda x: (self.DH(x)/self.DM(x))/(fidcosmo['DH'](x)/fidcosmo['DM'](x))
+
+        self.DV_rd     = lambda x: self.DV(x)/self.rdrag
+        self.DM_DH     = lambda x: self.DM(x)/self.DH(x)
+        self.DM_rd     = lambda x: self.DM(x)/self.rdrag
+        self.DH_rd     = lambda x: self.DH(x)/self.rdrag
 
         ###############
         #Computing DDR#
@@ -116,8 +145,9 @@ class TheoryCalcs:
 
     def call_custom(self,params):
 
-        hubble = params['H0']*np.sqrt(params['omegam']*(1+self.zcalc)**(3*params['Delta'])+(1-params['omegam']))
-        Hz = interp1d(self.zcalc,hubble/clight)
+        zfine  = np.linspace(min(self.zcalc),max(self.zcalc),len(self.zcalc)*10)
+        hubble = params['H0']*np.sqrt(params['omegam']*(1+zfine)**(3+params['Delta'])+(1-params['omegam']))
+        Hz = interp1d(zfine,hubble/clight)
 
         comov_vec = []
         for z in self.zcalc:
@@ -128,7 +158,7 @@ class TheoryCalcs:
         rdrag = 1. #MM: to be updated
 
         theory = {'H_Mpc': Hz,
-                  'H_kmsMpc': interp1d(self.zcalc,hubble),
+                  'H_kmsMpc': interp1d(zfine,hubble),
                   'DM': interp1d(self.zcalc,comov),
                   'DH': interp1d(self.zcalc,1/(Hz(self.zcalc))),
                   'DV': interp1d(self.zcalc, (self.zcalc*comov**2/Hz(self.zcalc))**(1/3)),